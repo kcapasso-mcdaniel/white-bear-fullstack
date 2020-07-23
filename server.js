3abbfc8fbdb1d5f50822312d5e5c390f35d8a70b
kcapasso-mcdaniel <64052620+kcapasso-mcdaniel@users.noreply.github.com>
2020-07-23 12:18:29 -0700

    add heroku deployment route

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 // express
 const express = require("express");
-
 const app = express();
+const path = require("path");
 
 // Routes
 
 app.use("/api/v1/users", require("./api/v1/users"));
 app.use("/api/v1/memory-cards", require("./api/v1/memory-cards"));
-app.get("/", (req, res) => res.send("Hello World!"));
+
+// if not route is built use build folder
+app.use(express().static("client/build"));
+app.get("*", (req, res) => {
+   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 // keep port as an environment variable
 const port = process.env.PORT || 5000;
